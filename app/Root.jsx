fb5d4c47405f25b442c101568a7449ef9df9ce09
Pavlos Polianidis <ppoliani@gmail.com>
2017-04-30 16:01:21 +0100

    change dev tools

--- app/Root.jsx
+++ app/Root.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
@@ -8,7 +7,7 @@ import App from './App';
 export default class Root extends Component {
   render() {
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+      <MuiThemeProvider>
         <App />
       </MuiThemeProvider>
     );
